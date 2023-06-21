7a32974aeba4337e137e17ec2fca596feb84990b
Dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2023-06-20 19:08:22 -0600

    guardo string del archivo en variable

--- main.asm
+++ main.asm
@@ -1,5 +1,4 @@
 include utils.asm
-
 .radix 16
 .model small
 .stack
@@ -9,10 +8,22 @@ include utils.asm
     credentialFileConfirmation db "EXISTE EL ARCHIVO","$"
     credentialFileNotExistent  db "CREDENCIALES NO ENCONTRADAS","$"
     newLine db 0A, "$"
+    quote db '"'
     ; variables para archivos
 
     handlecredentialFile dw 0000
     pathcredentialFile db "asm\pra\PRAII.CON",0 ; la ruta donde asm tiene que buscar
+
+
+    ; buffers
+    readBuffer db 256 dup(0), "$"
+
+    ; ESTRUCTURAS
+    codigoProducto db 04 dup(0)
+    descProducto db 32 dup(0)
+    productPrice db 02 dup(0)
+    productUnits db 02 dup(0)
+    
 .code
 
     main PROC
@@ -23,27 +34,40 @@ include utils.asm
     ; imprimir el encabezado
     printString mensajeBienvenida
 
-
+    ; ABRE EL ARCHIVO PRAII.CON
     mov AL, 02
-		mov AH, 3d
-		mov DX, offset pathcredentialFile
-		int 21
-		;; si no lo cremos
-		jc  credentialFileFailed
-		;; si abre escribimos
-		jmp credentialFileSuccess
+	mov AH, 3d
+	mov DX, offset pathcredentialFile
+	int 21
+    ;--------------------------------
+	; si no lo logra abrir, entonces imprimimos mensaje de fallo
+	jc  credentialFileFailed ; jc - jump if condition met, es decir, salta si se abrio el archivo
+	; si lo logra abrir mostramos menu inicial
+    mov [handlecredentialFile] , AX
+   
+	jmp credentialFileSuccess
     
     credentialFileFailed:
         printString newLine
         printString credentialFileNotExistent
-         jmp exit
+        jmp exit
     
     credentialFileSuccess:
         printString newLine
         printString credentialFileConfirmation
-       
-    
-
+        ; guardamos el handle
+        
+    readCredentialFile:
+        mov BX, [handlecredentialFile]
+        mov CX, 254d
+        mov DX, offset readBuffer 
+        mov AH, 3F
+        int 21
+    closeCredentialFile:
+        mov BX, [handlecredentialFile]
+        mov AH, 3e
+        int 21
+        printString readBuffer
     
 
     
